c55ce5ede7187273536f2ff5c2336bf3dcdc2f81
jayboyyy92 <jsvr159@gmail.com>
2025-05-13 09:06:07 +1000

    Fix getHumanChoice syntax, initialize scores, stub playRound logic, and add basic console logging

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
-function getComputerChoice() {
-    let num = Math.floor(Math.random() * 3);
+function getComputerChoice(){
+  let num = Math.floor(Math.random() * 3);
   
-    if (num === 0) {
-      return "rock";
-    } else if (num === 1) {
-      return "paper";
-    } else {
-      return "scissors";
-    }
+  if (num === 0) {
+    return 'rock';
+  } else if (num === 1) {
+    return 'paper';
+  } else {
+    return 'scissors';
   }
+}
 
-  console.log(getComputerChoice());
-  console.log(getComputerChoice());
-  console.log(getComputerChoice());
+console.log(getComputerChoice());
+
+function getHumanChoice(){
+  return let = prompt('rock, paper, or scissors');
+
+}
+
+console.log(getHumanChoice());
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice){
+
+  return humanScore.toLowerCase();
+  return  
+
+
+}
+
+console.log('You lose! Paper beats Rock')
+
+playRound(humanSelection, computerSelection);
